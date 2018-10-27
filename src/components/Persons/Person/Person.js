16b746b5a8e76f557d6dcfb4583b96d42ac39d7e
Pulkit Saxena <psaksena85@gmail.com>
2018-10-27 11:35:42 -0400

    Convert stateless to statefull

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,24 +1,16 @@
-import React from 'react';
+import React, {Component} from 'react';
 import classes from './Person.css';
 
-const person = (props) => {
-  // const styles = {
-  //   '@media (min-width:1200px) and (max-width:1440px)': {
-  //     width: '450px'
-  //   },
-
-  //   '@media (min-width:1441px)': {
-  //     width: '600px'
-  //   },
-  // };
-
-  return (
-    <div className={classes.Person}>
-      <p onClick={props.click}>I'm {props.name} and I'm {props.age} years old.</p>
-      <p>{props.children}</p>
-      <input type="text" onChange={props.changed} value={props.name}/>
-    </div>
-    );
+class Person extends Component {
+  render () {
+    return (
+      <div className={classes.Person}>
+        <p onClick={this.props.click}>I'm {this.props.name} and I'm {this.props.age} years old.</p>
+        <p>{this.props.children}</p>
+        <input type="text" onChange={this.props.changed} value={this.props.name}/>
+      </div>
+      );
+  }
 }
 
-export default person;
+export default Person;
